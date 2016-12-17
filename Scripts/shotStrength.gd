df9981dd6168091739110a2e685a01cae654c089
cypher31 <kelbystyler@gmail.com>
2016-12-17 15:20:36 -0800

    Moved some scenes around

--- Scripts/shotStrength.gd
+++ Scripts/shotStrength.gd
@@ -1,10 +1,28 @@
 extends Control
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var lineStartPosition
+var lineEndPosition
+
+func _input(event):
+	
+	if(event.is_action_pressed("shoot")):
+		lineStartPosition = get_local_mouse_pos()
+
+func _draw():
+	
+	if(get_parent().drawLine == true):
+		draw_line(lineStartPosition, lineEndPosition, Color(255,0,0), 2)
+	
+	pass
+	
+func _process(delta):
+	
+	if(get_parent().drawLine == true):
+		lineEndPosition = get_local_mouse_pos()
+	update()
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_process(true)
+	set_process_input(true)
+
+	
